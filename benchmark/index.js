b0019546c9c19e0bbbe38c1e1bb8ee9581df56ff
Jannis R <mail@jannisr.de>
2019-08-04 02:43:16 +0200

    decoding benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -4,18 +4,26 @@ const normalize = require('normalize-for-search')
 const {Suite} = require('benchmark')
 
 const build = require('../build')
-const create = require('..')
 const data = require('./data.json') // 13k stations
+const encode = require('../encode')
+const create = require('..')
+const decode = require('../decode')
 
 const tokenize = str => normalize(str).replace(/[^\w\s]/g, '').split(/\s+/g)
 
-const {tokens, scores, weights, nrOfTokens, originalIds} = build(tokenize, data)
+const index = build(tokenize, data)
+const encodedIndex = encode(index)
+
+const {tokens, scores, weights, nrOfTokens, originalIds} = index
 const autocomplete = create(tokens, scores, weights, nrOfTokens, originalIds, tokenize)
 
 new Suite()
 
 .add('13k VBB stations, "friedrich", 3 results, completion', () => {
 	autocomplete('friedrich', 3, false, true)
+})
+.add('index decoding', () => {
+	decode(encodedIndex)
 })
 
 .on('error', (err) => {
